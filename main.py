cb06a088d4f2260f4b6b5839e1dd8a4ffb73dbfa
Roman Peters <github@romanpeters.nl>
2020-02-11 14:40:08 +0100

    Add missing participants

--- main.py
+++ main.py
@@ -74,34 +74,83 @@ def command(msg):
         bot.sendMessage(chat_id, "No doodle saved")
         return
 
-    # chat_entry = session.query(db.Chat).filter_by(chat_id=chat_id).first()
-    # users = chat_entry.users
-
-    # print(doodle_entry.url)
-    # print(users)
-    # for u in users:
-    #     print(u.first_name)
+    chat_entry = session.query(db.Chat).filter_by(chat_id=chat_id).first()
+    poll = doodle.Doodle(doodle_entry.url)
+    message = DoodleMessage(poll=poll, chat_entry=chat_entry).get_message()
 
     session.close()
+    print(message)
 
-    poll = doodle.Doodle(doodle_entry.url)
+    bot.sendMessage(chat_id, message, parse_mode="Markdown", disable_web_page_preview=True)
 
-    # Check for poll
-    if not poll.json_file:
-        bot.sendMessage(chat_id, "Poll not found")
-        return
 
-    title = f"*{poll.get_title()}*"
-    participants = "\n\U00002611".join(['']+poll.get_participants())
+class DoodleMessage(object):
+    def __init__(self, poll, chat_entry):
+        self.poll: doodle.Doodle = poll
+        self.chat_entry: db.Chat = chat_entry
+        self.chat_members = {u.user_id: u for u in chat_entry.users}
+        self.title: str = f"*{poll.get_title()}*"
+        self.participants: str = "\n\U00002611".join([''] + poll.get_participants()).strip()
+        self.final_dates: str = str([d[0].strftime('%A %d %B %H:%M').replace("00:00", "") for d in poll.get_final()])
+        self.missing: str = "\n\U000025FB".join([''] + self.get_missing()).strip()
 
-    # Check if poll is open
-    if not poll.is_open():
-        final_dates = [d[0].strftime('%A %d %B %H:%M').replace("00:00", "") for d in poll.get_final()]
-        bot.sendMessage(chat_id, "\n".join([title] + final_dates), parse_mode="Markdown")
-        return
+    def get_message(self):
+        if not self.poll.is_open():
+            return "\n".join([self.title, self.final_dates])
+        return f"{self.title}\n{self.poll.url}\n{self.participants}\n{self.missing}"
 
-    bot.sendMessage(chat_id, f"{title}\n{doodle_entry.url}\n{participants}", parse_mode="Markdown",
-                    disable_web_page_preview=True)
+    def get_missing(self) -> list:
+        """lists chat_members who did not participate in the doodle"""
+        chat_members = self.chat_members
+        participating = self.poll.get_participants()
+        for doodler in participating:
+            chat_id = self.identify(doodler)
+            try:
+                chat_members.pop(chat_id)
+            except KeyError:
+                pass
+        names = []
+        for user in chat_members.values():
+            names.append(f"@{user.username}" if user.username else user.first_name)
+        return names
+
+    def identify(self, name) -> int:
+        """Returns the chat_id of a user"""
+        score = float("inf")
+        most_likely = None
+        for chat_id, user in self.chat_members.items():
+            user_names = [user.username, user.first_name, user.last_name, f"{user.first_name} {user.last_name}"]
+            for user_name in user_names:
+                edit_distance = self.levenshtein(name, user_name)
+                if edit_distance == 0:  # perfect match
+                    return chat_id
+                if edit_distance < score:
+                    score = edit_distance
+                    most_likely = chat_id
+        return most_likely
+
+    def levenshtein(self, a, b):
+        s1, s2 = a.lower(), b.lower()
+        if len(s1) > len(s2):
+            s1, s2 = s2, s1
+
+        distances = range(len(s1) + 1)
+        for i2, c2 in enumerate(s2):
+            distances_ = [i2 + 1]
+            for i1, c1 in enumerate(s1):
+                if c1 == c2:
+                    distances_.append(distances[i1])
+                else:
+                    distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+            distances = distances_
+        return distances[-1]
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
